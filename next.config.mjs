d7a98df40095d14933fe72a8d2571d23233a6569
Awsome-obedeh <obedchidera1010@gmail.com>
2025-09-06 14:04:57 +0100

    fixed build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+
+            },
+        ],
+    },
+
+    experimental: {
+        appDir: true, // make sure App Router is enabled
+    },
+    // This makes Next.js look in src/ for app or pages
+    srcDir: true,
+
 };
 
 export default nextConfig;
